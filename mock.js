153bfd4f9b9909669b8428fb68384cf14b7782a2
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-06-20 11:50:30 +0300

    file content

--- mock.js
+++ mock.js
@@ -5,6 +5,8 @@ const MOCK_DIR = './large/src/lib/mock'
 const MOCK_FILE_INDEX = path.join(MOCK_DIR, 'index.tsx')
 const MOCK_ITEMS_DIR = path.join(MOCK_DIR, 'item')
 
+const LENGTH_FILE_IMPORTS = 1;
+
 console.log('Mock', MOCK_FILE_INDEX)
 
 const getItemsSize = () => {
@@ -14,8 +16,20 @@ const getItemsSize = () => {
   return size.length
 }
 
+const generateIndexFileContent = () => {
+  const items = fs.readdirSync(MOCK_ITEMS_DIR)
+  const result = items.
+    slice(MOCK_FILE_INDEX)
+    .map(item => item.replace('.tsx', ''))
+    .map(fileName => `export * from './item/${fileName};`)
+    .map((line, index) => index < LENGTH_FILE_IMPORTS ? line : '\\\\ ' + line)
+    .join('\n')
+  console.log(result)
+  return result
+}
+
 const updateIndexFile = () => {
-  console.log(`Update ${MOCK_FILE_INDEX}`)
+  console.log(`Update ${MOCK_FILE_INDEX}`, LENGTH_FILE_IMPORTS)
   getItemsSize()
   fs.readFile(MOCK_FILE_INDEX, 'utf8', (err, data) => {
     console.log(data)
@@ -23,4 +37,5 @@ const updateIndexFile = () => {
   })
 }
 
-updateIndexFile()
+// updateIndexFile()
+generateIndexFileContent()

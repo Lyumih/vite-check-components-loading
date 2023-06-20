016242be8cd7021d1efdcb33b07ef13106807dda
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-06-20 11:26:37 +0300

    update file

--- mock.js
+++ mock.js
@@ -1,1 +1,18 @@
-console.log('Mock')
+const fs = require('fs');
+const path = require('path');
+
+const MOCK_DIR = './large/src/lib/mock'
+const MOCK_FILE_INDEX = path.join(MOCK_DIR, 'index.tsx')
+
+console.log('Mock', MOCK_FILE_INDEX)
+
+
+const updateIndexFile = () => {
+  console.log(`Update ${MOCK_FILE_INDEX}`)
+  fs.readFile(MOCK_FILE_INDEX, 'utf8', (err, data) => {
+    console.log(data)
+    console.log(data.split('\n'))
+  })
+}
+
+updateIndexFile()

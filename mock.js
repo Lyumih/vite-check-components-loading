7bc6c39706700fad63f01c068776d87a53bea58c
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-06-20 11:35:15 +0300

    up

--- mock.js
+++ mock.js
@@ -3,12 +3,20 @@ const path = require('path');
 
 const MOCK_DIR = './large/src/lib/mock'
 const MOCK_FILE_INDEX = path.join(MOCK_DIR, 'index.tsx')
+const MOCK_ITEMS_DIR = path.join(MOCK_DIR, 'item')
 
 console.log('Mock', MOCK_FILE_INDEX)
 
+const getItemsSize = () => {
+  const size = fs.readdirSync(MOCK_ITEMS_DIR).length
+  console.log('Items size: ' + size)
+
+  return size.length
+}
 
 const updateIndexFile = () => {
   console.log(`Update ${MOCK_FILE_INDEX}`)
+  getItemsSize()
   fs.readFile(MOCK_FILE_INDEX, 'utf8', (err, data) => {
     console.log(data)
     console.log(data.split('\n'))
